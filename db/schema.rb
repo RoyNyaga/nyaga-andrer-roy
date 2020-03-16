66baab5bd1ade238198ea6cfe84a4308e4f44aec
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-16 23:07:19 +0100

    add active_admin gem and create resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_210216) do
+ActiveRecord::Schema.define(version: 2020_03_16_211955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +46,10 @@ ActiveRecord::Schema.define(version: 2020_03_16_210216) do
     t.string "description"
     t.string "detailed_description"
     t.string "photo"
-    t.string "technology"
+    t.string "live_demo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "github_link"
   end
 
   create_table "technologies", force: :cascade do |t|
